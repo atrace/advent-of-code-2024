0a80674e8bc2a27d1cadf73c014be3374fff3f4a
Anjali Trace <anjali.trace2@nhs.net>
2025-12-02 12:16:04 +0000

    day 1 attempt for python practice

--- day1/day1_script.py
+++ day1/day1_script.py
@@ -2,26 +2,62 @@ input_file = "day1/day1_input.txt"
 line_separator = "   "
 
 
-location_list_1 = []
-location_list_2 = []
-total_distance = 0
+def part_1():
+    location_list_1 = []
+    location_list_2 = []
+    total_distance = 0
 
-with open(input_file) as file:
-    for line in file:
-        locations = line.strip().split(line_separator)
-        location_list_1.append(int(locations[0]))
-        location_list_2.append(int(locations[1]))
+    with open(input_file) as file:
+        for line in file:
+            locations = line.strip().split(line_separator)
+            location_list_1.append(int(locations[0]))
+            location_list_2.append(int(locations[1]))
 
-print("🚀 list length 1", len(location_list_1))
-print("🚀 list length 2", len(location_list_2))
+    print("🚀 list length 1", len(location_list_1))
+    print("🚀 list length 2", len(location_list_2))
 
-location_list_1.sort()
-location_list_2.sort()
+    location_list_1.sort()
+    location_list_2.sort()
 
-for index in range(len(location_list_1)):
-    location1 = location_list_1[index]
-    location2 = location_list_2[index]
+    for index in range(len(location_list_1)):
+        location1 = location_list_1[index]
+        location2 = location_list_2[index]
 
-    total_distance += abs(location1 - location2)
+        total_distance += abs(location1 - location2)
 
-print("🚀 total_distance", total_distance)
+    print("🚀 total_distance", total_distance)
+
+
+def part_2():
+    location_list_1 = []
+    location_list_2 = []
+    similarity_score = 0
+
+    with open(input_file) as file:
+        for line in file:
+            locations = line.strip().split(line_separator)
+            location_list_1.append(int(locations[0]))
+            location_list_2.append(int(locations[1]))
+
+    print("🚀 list length 1", len(location_list_1))
+    print("🚀 list length 2", len(location_list_2))
+
+    similarity_frequency = {}
+
+    for location in location_list_1:
+        # score = similarity_frequency.get(location)
+        if location in similarity_frequency:
+            similarity_frequency[location] += location
+        else:
+            similarity_frequency[location] = location
+
+    print("🚀 similarity_frequency", similarity_frequency)
+    print("🚀 location_list_1", location_list_1)
+    for location in location_list_1:
+        similarity_score += similarity_frequency[location]
+        print("🚀 similarity_frequency[location]", similarity_frequency[location])
+
+    print("🚀 similarity_score", similarity_score)
+
+
+part_2()
